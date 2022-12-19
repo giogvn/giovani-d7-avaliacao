7b39518694bb386d8c0872bae95e2937df6b893f
unknown <giovanitavares@outlook.com>
2022-12-19 01:43:56 -0300

    Finishes final avaliation
    Signed-off-by: unknown <giovanitavares@outlook.com>

---
+++ SimpleLogin/MainWindow.xaml.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using SimpleLogin.Models;
+using SimpleLogin.Repositories;
+
+
+namespace SimpleLogin
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        User possibleUser = new();
+        public MainWindow()
+        {
+            InitializeComponent();
+            PossibleUserGrid.DataContext = possibleUser;
+        }
+
+        private void Login(object sender, RoutedEventArgs e)
+        {
+            UserRepository saverRepository = new UserRepository();
+
+            List<User> users = saverRepository.findUser(possibleUser.Email, possibleUser.Password);
+            if (users.Count > 0)
+            {
+                User saver = users[0];
+                MessageBox.Show("Usuário autenticado!");
+            }
+            else
+            {
+                MessageBox.Show("Credenciais Inválidas!");
+            }
+        }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ SimpleLogin/Repositories/UserRepository.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using SimpleLogin.Models;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+
+namespace SimpleLogin.Repositories
+{
+    public class UserRepository
+    {
+        static string ConnectionString = "Server=labsoft.pcs.usp.br; Initial Catalog=db_7; User id=usuario_7; pwd=44192792818;";
+
+        public void Create(User newRecord)
+        {
+            string queryInsert = $"INSERT INTO Users (email, password) VALUES (@Email, @Password)";
+            using (SqlConnection con = new(ConnectionString))
+            {
+                using (SqlCommand cmd = new SqlCommand(queryInsert, con))
+                {
+                    cmd.Parameters.AddWithValue("@Email", newRecord.Email);
+                    cmd.Parameters.AddWithValue("@Password", newRecord.Password);
+                    con.Open();
+                    cmd.ExecuteNonQuery();
+                }
+            }
+        }
+
+        public List<User> findUser(string email, string password)
+        {
+            List<User> list = new();
+            string querySelect = "SELECT * FROM Users WHERE email = @Email AND password = @Password";
+            using (SqlConnection con = new(ConnectionString))
+            {
+                con.Open();
+                SqlDataReader rdr;
+                using (SqlCommand cmd = new SqlCommand(querySelect, con))
+                {
+                    cmd.Parameters.AddWithValue("@Email", email);
+                    cmd.Parameters.AddWithValue("@Password", password);
+                    try
+                    {
+                        rdr = cmd.ExecuteReader();
+                        rdr.Read();
+                        User record = new User()
+                        {
+                            Email = rdr["email"].ToString(),
+                            Password = rdr["password"].ToString()
+                        };
+                        list.Add(record);
+                        return list;
+                    }
+                    catch (Exception e)
+                    {
+                        return list;
+                    }
+                }
+            }
+        }
+    }
+}
